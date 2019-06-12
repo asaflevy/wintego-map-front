d079d1167edf1a0ad56ebb45e6edcbe4f317009f
asafart <asaf@artlist.io>
2019-06-13 01:06:22 +0300

    fix for env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://wintego-map-backend.herokuapp.com/',
+  baseUrl: 'https://wintego-map-backend.herokuapp.com',
   baseApiUrl: 'https://wintego-map-backend.herokuapp.com/api'
 };
