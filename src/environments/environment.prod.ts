1bfee17e5d1c5b2e37b6dd489c38b3f409a8ce02
asafart <asaf@artlist.io>
2019-06-13 00:47:56 +0300

    adding env for heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/',
-  baseApiUrl: 'http://localhost:3000/api'
+  baseUrl: 'https://wintego-map-backend.herokuapp.com/',
+  baseApiUrl: 'https://wintego-map-backend.herokuapp.com/api'
 };
